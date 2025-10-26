4856767e65e8b50586c3c3e2b9e4fa100c8c232e
MLZ6 <kopomasuga64@gmail.com>
2025-10-26 09:31:32 +0200

    added current location

--- src/index.js
+++ src/index.js
@@ -35,8 +35,10 @@ honoluluDateElement.innerHTML = honoluluTime.format("MMMM Do YYYY");
 honoluluTimeElement.innerHTML = honoluluTime.format("h:mm:ss [<small>]A[</small>]");}
 }
 function updateCity(event) {
-
-  let cityTimeZone = event.target.value; 
+ let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
